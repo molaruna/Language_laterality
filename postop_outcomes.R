438e7b9f0342d2675f2ade3d5dffaa376c3f00da
molaruna <36777247+molaruna@users.noreply.github.com>
2020-11-10 09:21:22 -0800

    Delete postop_outcomes.R

--- postop_outcomes.R
+++
@@ -1,4 +0,0 @@
-library(scales)
-post_df = read.csv("~/google_drive/LPN_personal/studies/Lang/tables/Lang_postop_outcomes.csv")
-
-post_df$clin.score.rescale = rescale(post_df$clin.score, to = c(-1, 1), from = range(post_df$clin.score, na.rm = TRUE, finite = TRUE))
